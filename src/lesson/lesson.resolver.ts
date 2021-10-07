a3a91867d5a440b0499fe85cbd6b28daeb2592cf
manak1 <mana23happy@hotmail.co.jp>
2021-10-08 00:42:24 +0900

    :sparkles: feat: lesson取得/作成処理の追加

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,15 +1,26 @@
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Mutation, Query, Resolver } from '@nestjs/graphql';
+import { create } from 'domain';
+import { CreateLessonInput } from './lesson.input';
+import { LessonService } from './lesson.service';
 import { LessonType } from './lesson.type';
 
 @Resolver((of) => LessonType)
 export class LessonResolver {
+  constructor(private lessonService: LessonService) {}
   @Query((returns) => LessonType)
-  lesson() {
-    return {
-      id: 'asasasas',
-      name: 'physics class',
-      startDate: new Date().toISOString(),
-      endDate: new Date().toISOString(),
-    };
+  lesson(@Args('id') id: string) {
+    return this.lessonService.getLesson(id);
+  }
+
+  @Query((returns) => [LessonType])
+  getAllLesson() {
+    return this.lessonService.getAllLesson();
+  }
+
+  @Mutation((returns) => LessonType)
+  createLesson(
+    @Args('createLessonInput') createLessonInput: CreateLessonInput,
+  ) {
+    return this.lessonService.createLesson(createLessonInput);
   }
 }
---
+++ src/lesson/lesson.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Lesson } from './lesson.entity';
+import { v4 as uuid } from 'uuid';
+import { CreateLessonInput } from './lesson.input';
+
+@Injectable()
+export class LessonService {
+  constructor(
+    @InjectRepository(Lesson) private lessonRepository: Repository<Lesson>,
+  ) {}
+
+  async createLesson(createLessonInput: CreateLessonInput): Promise<Lesson> {
+    const lesson = this.lessonRepository.create({
+      id: uuid(),
+      ...createLessonInput,
+    });
+    return this.lessonRepository.save(lesson);
+  }
+
+  async getLesson(id: string) {
+    return this.lessonRepository.findOne({ id });
+  }
+
+  async getAllLesson() {
+    return this.lessonRepository.find();
+  }
+}
